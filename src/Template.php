553688d000edcf5f3a3c3b6f9b7c89cb965ad585
Pavel Selitskas <p.selitskas@gmail.com>
2016-10-05 19:18:09 +0300

    Annotate AlexaCRM\WordpressCRM\Template

--- src/Template.php
+++ src/Template.php
@@ -6,20 +6,42 @@ if ( !defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
+/**
+ * Provides access to templates
+ *
+ * @package AlexaCRM\WordpressCRM
+ */
 class Template {
 
+    /**
+     * Prints or returns template body.
+     *
+     * @param string $template_name
+     * @param array $args Array of variables to make available inside the template
+     * @param string $template_path Path to the template
+     * @param string $default_path Default path to the template
+     * @param bool $echo Output the template to browser if true, return it as string otherwise
+     *
+     * @return string|void
+     */
     public static function printTemplate( $template_name, $args = array(), $template_path = '', $default_path = '', $echo = false ) {
         ob_start();
         Template::getTemplate( $template_name, $args, $template_path, $default_path );
-        if ( $echo ) //echo trim(preg_replace('/[\t\n\r\0\x0B]/', '', ob_get_clean()));
-        {
+        if ( $echo ) {
             echo ob_get_clean();
-        } else //return trim(preg_replace('/[\t\n\r\0\x0B]/', '', ob_get_clean()));
-        {
+        } else {
             return ob_get_clean();
         }
     }
 
+    /**
+     * Prints the template body.
+     *
+     * @param string $template_name
+     * @param array $args Array of variables to make available inside the template
+     * @param string $template_path Path to the template
+     * @param string $default_path Default path to the template
+     */
     public static function getTemplate( $template_name, $args = array(), $template_path = '', $default_path = '' ) {
         if ( $args && is_array( $args ) ) {
             extract( $args );
@@ -43,6 +65,15 @@ class Template {
         do_action( 'wordpresscrm_after_template_part', $template_name, $template_path, $located, $args );
     }
 
+    /**
+     * Locates the template file.
+     *
+     * @param string $template_name
+     * @param string $template_path
+     * @param string $default_path
+     *
+     * @return string Full path to the template file
+     */
     public static function locateTemplate( $template_name, $template_path = '', $default_path = '' ) {
         if ( !$template_path ) {
             $template_path = ACRM()->template_path();
@@ -69,6 +100,16 @@ class Template {
         return apply_filters( 'wordpresscrm_locate_template', $template, $template_name, $template_path );
     }
 
+    /**
+     * Constructs the path to the shortcode template.
+     *
+     * @param string $path
+     * @param string $firstPart
+     * @param string $secondPart
+     * @param string $thirdPart
+     *
+     * @return string
+     */
     public static function locateShortcodeTemplate( $path, $firstPart = null, $secondPart = null, $thirdPart = null ) {
 
         $pathFirst            = ( $firstPart ) ? $path . "-" . strtolower( str_replace( " ", "-", $firstPart ) ) : null;
