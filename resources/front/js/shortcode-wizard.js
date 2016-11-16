4f517496f2dd0a5fd40dead23767e88942f708a9
Pavel Selitskas <p.selitskas@gmail.com>
2016-11-16 14:19:49 +0300

    Basic support for view shortcode in the Shortcode Wizard

--- resources/front/js/shortcode-wizard.js
+++ resources/front/js/shortcode-wizard.js
@@ -10,6 +10,29 @@
      */
     var Shortcode = Backbone.Model.extend( {
 
+        initialize: function() {
+            _.bind( this.generateCode, this );
+        },
+
+        generateCode: function() {
+            var args = {}, shortcode = this;
+
+            this.fields.forEach( function( field ) {
+                args[field.get( 'name' )] = field.getFieldValue();
+            } );
+
+            wp.ajax.post( 'wpcrm_sw_result', {
+                name: this.get('name'),
+                fields: args
+            } )
+                .done( function( response ) {
+                    shortcode.view.updateResult( response.result );
+                } )
+                .fail( function( response ) {
+                    shortcode.view.updateResult( 'Error: ' + response.message );
+                } );
+        }
+
     } );
 
     /**
@@ -17,10 +40,94 @@
      */
     var ShortcodeField = Backbone.Model.extend( {
 
+        initialize: function() {
+            this.setFieldValue( null );
+
+            var providingFields = this.get( 'value' ).args;
+            if ( providingFields && providingFields.length ) {
+                _.each( providingFields, function( fieldName ) {
+                    var field = this.collection.findWhere( { name: fieldName } ),
+                        dependingField = this;
+
+                    if ( !field ) {
+                        return;
+                    }
+
+                    field.on( 'change:controlValue', function() {
+                        var args = {};
+                        args[field.get( 'name' )] = field.getFieldValue();
+                        dependingField.updatedProvidingField( args );
+                    }, this )
+                }, this );
+            }
+
+            _.bind( this.getView, this );
+        },
+
+        getValues: function() {
+            var model = this, valueSettings, args = {};
+
+            valueSettings = this.get( 'value' );
+            if ( valueSettings.source !== 'ajax' ) {
+                return [];
+            }
+
+            if ( valueSettings.args && valueSettings.args.length ) {
+                _.each( valueSettings.args, function( fieldName ) {
+                    args[fieldName] = model.collection.findWhere( { name: fieldName } ).getFieldValue();
+                }, this );
+            }
+
+            return wp.ajax.post( valueSettings.action, args );
+        },
+
+        getFieldValue: function() {
+            return this.get( 'controlValue' );
+        },
+
+        setFieldValue: function( value ) {
+            this.set( 'controlValue', value );
+
+            return this;
+        },
+
+        updatedProvidingField: function( updatedFields ) {
+            this.getView().render();
+        },
+
+        getView: function() {
+            if ( this.view ) {
+                return this.view;
+            }
+
+            switch ( this.get( 'type' ) ) {
+                case 'dropdown':
+                    this.view = new ShortcodeDropdownFieldView( { model: this } );
+                    break;
+                default:
+                    this.view = new ShortcodeFieldView( { model: this } );
+                    break;
+            }
+
+            return this.view;
+        }
+
     } );
 
     var ShortcodeFieldCollection = Backbone.Collection.extend( {
-        model: ShortcodeField
+        model: ShortcodeField,
+
+        initialize: function( attributes, options ) {
+            var fields = this;
+
+            if ( options && options.shortcode ) {
+                this.shortcode = options.shortcode;
+            }
+
+            this.on( 'change:controlValue', function( field, value ) {
+                this.shortcode.generateCode();
+            } );
+        }
     } );
 
     /**
@@ -30,29 +137,141 @@
         model: Shortcode
     } );
 
+    var shortcodes = new ShortcodeCollection();
+
+    _.each( window.wpcrmShortcodeWizard.shortcodes, function( shortcodeDefinition, shortcodeName ) {
+        var fields = new ShortcodeFieldCollection(), newShortcode;
+
+        _.each( shortcodeDefinition.fields, function( fieldDefinition, fieldName ) {
+            fields.add( new ShortcodeField( fieldDefinition, { collection: fields } ) );
+        } );
+
+        newShortcode = new Shortcode(
+            _.extend( shortcodeDefinition ),
+            { collection: shortcodes }
+        );
+
+        fields.shortcode = newShortcode;
+        newShortcode.fields = fields;
+
+        shortcodes.add( newShortcode );
+    } );
+
+    /**
+     * Shortcode Wizard view.
+     */
     var ShortcodeWizardView = Backbone.View.extend( {
 
         template: _.template( $( '#tpl-wpcrmShortcodeWizard' ).html() ),
 
+        events: {
+            'change .wpcrm-sw-selector': 'select'
+        },
+
         render: function() {
-            console.log( this.$el );
-            this.$el.html( this.template() );
+            this.$el.html( this.template( { shortcodes: this.collection } ) );
+            return this;
+        },
+
+        select: function() {
+            var $selector = this.$el.find( '.wpcrm-sw-selector' ),
+                $description = this.$el.find( '.wpcrm-sw-container .shortcode-description' ),
+                selectedShortcode;
+
+            if ( !$selector.val() ) {
+                this.$el.find( '.wpcrm-sw-container' ).hide();
+                return;
+            }
+
+            this.$el.find( '.wpcrm-sw-container' ).show();
+
+            selectedShortcode = this.collection.findWhere( { name: $selector.val() } );
+
+            if ( !selectedShortcode ) {
+                return;
+            }
+
+            $description.find( '.name' ).text( selectedShortcode.get( 'displayName' ) );
+            $description.find( '.description' ).text( selectedShortcode.get( 'description' ) );
+
+            if ( !selectedShortcode.view ) {
+                selectedShortcode.view = new ShortcodeView( { model: selectedShortcode } );
+            }
+
+            this.$el.find( '.wpcrm-sw-container .shortcode-container' ).append( selectedShortcode.view.render().$el );
+        }
+
+    } );
+
+    /**
+     * Shortcode view.
+     */
+    var ShortcodeView = Backbone.View.extend( {
+
+        template: _.template( $( '#tpl-wpcrmShortcodeWizardShortcode' ).html() ),
+
+        render: function() {
+            this.$el.html( this.template( { fields: this.model.fields } ) );
+            return this;
+        },
+
+        updateResult: function( result ) {
+            this.$el.find( '.shortcode-result' ).show().find( 'textarea' ).val( result );
+
             return this;
         }
 
     } );
 
-    var shortcodes = new ShortcodeCollection();
+    /**
+     * Shortcode field view.
+     */
+    var ShortcodeFieldView = Backbone.View.extend( {
 
-    _.each( window.wpcrmShortcodeWizard.shortcodes, function( shortcodeDefinition, shortcodeName ) {
-        var fields = new ShortcodeFieldCollection();
-        _.each( shortcodeDefinition.fields, function( fieldDefinition, fieldName ) {
-            fields.add( new ShortcodeField( fieldDefinition ) );
-        } );
+        template: _.template( $( '#tpl-wpcrmShortcodeWizardShortcodeField' ).html() ),
 
-        shortcodes.add( new Shortcode(
-            _.extend( shortcodeDefinition, { fields: fields } )
-        ) );
+        errorTemplate: _.template( $( '#tpl-wpcrmShortcodeWizardShortcodeFieldError' ).html() ),
+
+        loadingTemplate: _.template( $( '#tpl-wpcrmShortcodeWizardShortcodeFieldLoading' ).html() ),
+
+        render: function() {
+            this.$el.html( this.template( { field: this.model } ) );
+            return this;
+        }
+
+    } );
+
+    var ShortcodeDropdownFieldView = ShortcodeFieldView.extend( {
+
+        template: _.template( $( '#tpl-wpcrmShortcodeWizardShortcodeDropdownField' ).html() ),
+
+        events: {
+            'change .dropdown-value': 'valueChange'
+        },
+
+        render: function() {
+            var view = this;
+
+            view.$el.html( view.loadingTemplate( { fieldName: view.model.get( 'displayName' ) } ) );
+
+            this.model.getValues().done( function( values ) {
+                view.$el.html( view.template( { field: view.model, values: values } ) );
+            } )
+                .fail( function( response ) {
+                    view.$el.html( view.errorTemplate( { message: response.message } ) );
+                } );
+
+            return this;
+        },
+
+        valueChange: function() {
+            this.model.setFieldValue( this.getValue() );
+        },
+
+        getValue: function() {
+            return this.$el.find( '.dropdown-value' ).val();
+        }
+
     } );
 
     $( function() {
@@ -61,8 +280,6 @@
         } );
 
         $( '#wpcrmShortcodeWizard' ).append( wizardView.render().$el );
-
-        console.log( shortcodes );
     } );
 
     // export
--- src/Admin/Metabox/ShortcodeWizard.php
+++ src/Admin/Metabox/ShortcodeWizard.php
@@ -2,6 +2,8 @@
 
 namespace AlexaCRM\WordpressCRM\Admin\Metabox;
 
+use AlexaCRM\CRMToolkit\Entity\MetadataCollection;
+
 /**
  * Shortcode Wizard for the WordPress Post edit screen
  *
@@ -15,6 +17,11 @@ class ShortcodeWizard {
     public function __construct() {
         add_action( 'add_meta_boxes', [ $this, 'registerMetabox' ] );
         add_action( 'admin_enqueue_scripts', [ $this, 'enqueueScripts' ] );
+
+        // register AJAX handlers
+        add_action( 'wp_ajax_wpcrm_sw_result', [ $this, 'handlerResult' ] );
+        add_action( 'wp_ajax_wpcrm_sw_entities', [ $this, 'handlerEntities' ] );
+        add_action( 'wp_ajax_wpcrm_sw_entity_views', [ $this, 'handlerEntityViews' ] );
     }
 
     /**
@@ -47,6 +54,64 @@ class ShortcodeWizard {
         ?>
         <div id="wpcrmShortcodeWizard"></div>
 <script type="text/template" id="tpl-wpcrmShortcodeWizard">
+<p>
+    <label><?php _e( 'Pick a shortcode:', 'integration-dynamics' ); ?>
+        <select class="wpcrm-sw-selector">
+            <option value=""><?php _e( '— Select —', 'integration-dynamics' ); ?></option>
+            <% shortcodes.forEach( function( shortcodeDefinition ) {
+            %><option value="<%- shortcodeDefinition.get( 'name' ) %>"><%- shortcodeDefinition.get( 'displayName' ) %></option><%
+            } ); %>
+        </select>
+    </label>
+</p>
+<div class="wpcrm-sw-container" style="border-top: 1px solid #eceeef; padding-top: 10px; display: none;">
+    <div class="shortcode-description">
+        <strong class="name"></strong>. <em class="description"></em>
+    </div>
+    <div class="shortcode-container">
+
+    </div>
+</div>
+</script>
+<script type="text/template" id="tpl-wpcrmShortcodeWizardShortcode">
+<% fields.forEach( function( field ) {
+    jQuery( '.wpcrm-sw-container .shortcode-container' ).append( field.getView().render().$el );
+} ); %>
+<div class="shortcode-result" style="border-top: 1px solid #eceeef; padding-top: 10px; display: none;">
+    <textarea style="width:100%" rows="6" readonly></textarea>
+    <p class="description"><?php _e( 'Copy the generated code and paste into the editor window.', 'integration-dynamics' ); ?></p>
+</div>
+</script>
+<script type="text/template" id="tpl-wpcrmShortcodeWizardShortcodeField">
+Ditch it.
+</script>
+<script type="text/template" id="tpl-wpcrmShortcodeWizardShortcodeDropdownField">
+<p>
+    <label>
+        <%- field.get( 'displayName' ) %><br>
+        <select class="dropdown-value">
+            <option value=""><?php _e( '— Select —', 'integration-dynamics' ); ?></option>
+            <% _.each( values, function( valueName, value ) { %>
+                    <option value="<%- value %>"><%- valueName %></option>
+            <% } ); %>
+        </select>
+    </label>
+</p>
+</script>
+<script type="text/html" id="tpl-wpcrmShortcodeWizardShortcodeFieldError">
+<p style="color:#f00;">
+    <% if ( message ) { %>
+    <?php printf( __( 'Error: %s', 'integration-dynamics' ), '<%- message %>' ); ?>
+    <% } else { %>
+    <?php _e( 'Unknown error occurred.', 'integration-dynamics' ); ?>
+    <% } %>
+</p>
+</script>
+<script type="text/html" id="tpl-wpcrmShortcodeWizardShortcodeFieldLoading">
+<p>
+    <%- fieldName %><br>
+    <img src="<?php echo ACRM()->getPluginURL(); ?>/resources/front/images/progress.gif" width="18" height="18" alt="Loading...">
+</p>
 </script>
 <?php
     }
@@ -55,7 +120,7 @@ class ShortcodeWizard {
      * Enqueues front-end scripting for the wizard.
      */
     public function enqueueScripts() {
-        $scriptPath = ACRM()->plugin_url() . '/resources/front/js/shortcode-wizard.js';
+        $scriptPath = ACRM()->getPluginURL() . '/resources/front/js/shortcode-wizard.js';
 
         wp_enqueue_script( 'wordpresscrm-shortcode-wizard', $scriptPath, [ 'jquery', 'underscore', 'backbone' ], false, true );
 
@@ -63,20 +128,124 @@ class ShortcodeWizard {
             'shortcodes' => [
                 'view' => [
                     'name' => 'view',
-                    'displayName' => 'View',
-                    'description' => 'Renders a Dynamics CRM View as a table.',
+                    'displayName' => __( 'View', 'integration-dynamics' ),
+                    'description' => __( 'Renders a Dynamics CRM View as a table.', 'integration-dynamics' ),
                     'fields' => [
                         'entity' => [
                             'name' => 'entity',
-                            'displayName' => 'Entity name',
-                            'description' => 'Name of the entity to display a view of.',
+                            'displayName' => __( 'Entity name', 'integration-dynamics' ),
+                            'description' => __( 'Name of the entity to display a view of.', 'integration-dynamics' ),
                             'type' => 'dropdown',
-                            'action' => 'wpcrm_sw_entities',
+                            'value' => [
+                                'source' => 'ajax',
+                                'action' => 'wpcrm_sw_entities',
+                            ],
+                        ],
+                        'view' => [
+                            'name' => 'view',
+                            'displayName' => __( 'Entity View name', 'integration-dynamics' ),
+                            'description' => __( 'Name of the view to display.', 'integration-dynamics' ),
+                            'type' => 'dropdown',
+                            'value' => [
+                                'source' => 'ajax',
+                                'action' => 'wpcrm_sw_entity_views',
+                                'args' => [ 'entity' ], // cannot use 'arguments' per JavaScript restrictions
+                            ],
                         ],
                     ],
                 ],
             ],
         ] );
+    }
+
+    public function handlerResult() {
+        if ( !array_key_exists( 'name', $_POST ) ) {
+            wp_send_json_error( [ 'message' => __( 'Invalid request - shortcode name is absent', 'integration-dynamics' ) ] );
+        }
+
+        if ( !array_key_exists( 'fields', $_POST ) ) {
+            wp_send_json_error( [ 'message' => __( 'Invalid request - shortcode arguments are absent', 'integration-dynamics' ) ] );
+        }
+
+        $shortcodeName = trim( $_POST['name'] );
+        $shortcodeFields = $_POST['fields'];
+
+        if ( !is_array( $shortcodeFields ) ) {
+            wp_send_json_error( [ 'message' => __( 'Invalid request - shortcode arguments must be an array', 'integration-dynamics' ) ] );
+        }
+
+        if ( $shortcodeName !== 'view' ) {
+            wp_send_json_error( [ 'message' => __( 'Shortcode is not supported', 'integration-dynamics' ) ] );
+        }
+
+        $resultTemplate = '[msdyncrm_view entity="%1$s" name="%2$s"]';
+
+        $result = sprintf( $resultTemplate, $shortcodeFields['entity'], $shortcodeFields['view'] );
+
+        wp_send_json_success( [ 'result' => $result ] );
+    }
+
+    /**
+     * Returns the list of CRM entities.
+     */
+    public function handlerEntities() {
+        try {
+            $entities = MetadataCollection::instance()->getEntitiesList();
+            asort( $entities );
+
+            wp_send_json_success( $entities );
+        } catch ( \Exception $e ) {
+            wp_send_json_error( [ 'message' => $e->getMessage() ] );
+        }
+    }
+
+    /**
+     * Returns the list of views for a CRM entity.
+     */
+    public function handlerEntityViews() {
+        $views = [];
+
+        $entityName = trim( $_POST['entity'] );
+
+        if ( $entityName === '' ) {
+            wp_send_json_error( [ 'message' => __( 'Empty entity name in the request', 'integration-dynamics' ) ] );
+        }
+
+        $entity = ASDK()->entity( $entityName );
+
+        $fetch = '<fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
+						<entity name="userquery">
+							<attribute name="name" />
+							<attribute name="returnedtypecode" />
+							 <filter type="and">
+								<condition attribute="returnedtypecode" operator="eq" value="' . $entity->metadata()->objectTypeCode . '" />
+							  </filter>
+						</entity>
+					  </fetch>';
+
+        $userQueries = ASDK()->retrieveMultiple( $fetch );
+
+        $fetch = '<fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
+						<entity name="savedquery">
+							<attribute name="name" />
+							<attribute name="returnedtypecode" />
+							 <filter type="and">
+								<condition attribute="returnedtypecode" operator="eq" value="' . $entity->metadata()->objectTypeCode . '" />
+							  </filter>
+						</entity>
+					  </fetch>';
+
+        $savedQueries = ASDK()->retrieveMultiple( $fetch );
+
+        $viewEntities = array_merge( $userQueries->Entities, $savedQueries->Entities );
+
+        foreach ( $viewEntities as $viewEntity ) {
+            $views[$viewEntity->displayname] = $viewEntity->displayname;
+        }
+
+        asort( $views );
+
+        wp_send_json_success( $views );
     }
 
 }
