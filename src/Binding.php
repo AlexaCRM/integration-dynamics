708d37995dd5de3df82971c430c2d3e4891c8b11
Pavel Selitskas <p.selitskas@gmail.com>
2016-12-16 14:13:27 +0300

    Add a filter for binding columns

--- src/Binding.php
+++ src/Binding.php
@@ -293,10 +293,7 @@ class Binding {
         global $post;
         $content = $post->post_content;
 
-        $fields = [];
-
-        preg_match_all( '~\{\{\s*currentrecord\.([a-z0-9_\.]+)~', $content, $inlineFields );
-        $fields = array_merge( $fields, $inlineFields[1] );
+        $fields = apply_filters( 'wordpresscrm_data_binding_columns', [], $content );
 
         $shortcodeRegex = get_shortcode_regex( [ Plugin::PREFIX . 'field' ] );
 
