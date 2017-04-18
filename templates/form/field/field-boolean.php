83872c7520bdea95f191eda56ad107e5d769925e
Pavel Selitskas <p.selitskas@gmail.com>
2017-04-18 15:07:32 +0300

    Display boolean checkboxes correctly

--- templates/form/field/field-boolean.php
+++ templates/form/field/field-boolean.php
@@ -18,21 +18,19 @@ if ( !defined( 'ABSPATH' ) ) {
     <?php endforeach; ?>
 <?php endif; ?>
 
-<?php if ( $classid == '{B0C6723A-8503-4FD7-BB28-C8A06AC933C2}' ) : ?>
-    <?php foreach ( $options as $option => $val ) : ?>
-        <div class="checkbox">
-            <label>
-                <input <?php if ( $value == (bool) $option ) {
-                    echo "checked='checked'";
-                } ?> type='checkbox' name='<?php echo $inputname; ?>'
-                     value='<?php echo $option; ?>' <?php echo( ( $disabled ) ? "disabled='disabled'" : "" ); ?> <?php echo( ( $readonly ) ? "readonly='readonly'" : "" ); ?>> <?php echo $val; ?>
-            </label>
-        </div>
-        <?php break; ?>
-    <?php endforeach; ?>
-<?php endif; ?>
+<?php if ( $classid == '{B0C6723A-8503-4FD7-BB28-C8A06AC933C2}' ) { ?>
+    <input type="hidden" name="<?php echo esc_attr( $inputname ); ?>" value="0">
+    <div class="checkbox">
+        <label>
+            <input <?php if ( $value ) {
+                echo 'checked="checked"';
+            } ?> type="checkbox" name="<?php echo esc_attr( $inputname ); ?>"
+                 value="1" <?php echo( ( $disabled ) ? 'disabled="disabled"' : '' ); ?> <?php echo( ( $readonly ) ? 'readonly="readonly"' : '' ); ?>>
+        </label>
+    </div><?php
+}
 
-<?php if ( $classid == '{3EF39988-22BB-4F0B-BBBE-64B5A3748AEE}' ) : ?>
+if ( $classid == '{3EF39988-22BB-4F0B-BBBE-64B5A3748AEE}' ) : ?>
     <select class='selectmenu crm-select form-control' name='<?php echo $inputname; ?>'
             id='<?php echo $inputname; ?>' <?php echo( ( $disabled ) ? "disabled='disabled'" : "" ); ?> <?php echo( ( $readonly ) ? "readonly='readonly'" : "" ); ?>>
         <option value=''></option>
