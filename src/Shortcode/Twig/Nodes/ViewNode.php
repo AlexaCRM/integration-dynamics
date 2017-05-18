a62c14c81d7aab90f171c65c34e5e75c59bf4b21
Pavel Selitskas <p.selitskas@gmail.com>
2017-05-18 12:46:29 +0300

    Revamp the {% view %} tag
    Construct entityview object similar to AdXStudio https://community.adxstudio.com/products/adxstudio-portals/documentation/configuration-guide/liquid-templates/objects/entityview/

--- src/Shortcode/Twig/Nodes/ViewNode.php
+++ src/Shortcode/Twig/Nodes/ViewNode.php
@@ -27,115 +27,22 @@ class ViewNode extends \Twig_Node {
      * @param Twig_Compiler $compiler
      */
     public function compile( Twig_Compiler $compiler ) {
-        /**
-         * @var \Twig_Node_Expression_Constant $entityName
-         */
-        $entityName = $this->getAttribute( 'entity' );
-        $viewName = $this->getAttribute( 'name' );
+        $compiler->write( "\$viewAttributes = [" );
+        end( $this->attributes );
+        $lastAttributeName = key( $this->attributes );
+        foreach ( $this->attributes as $attributeName => $attribute ) {
+            $compiler->write( "'{$attributeName}'=>");
+            $attribute->compile( $compiler );
 
-        $compiler->write( "\$entityName = " );
-        $entityName->compile( $compiler );
-        $compiler->write( ";\n" );
-        $compiler->write( "\$viewName = " );
-        $viewName->compile( $compiler );
-        $compiler->write( ";\n" );
-
-        // Add parameters
-        $compiler->write( "\$viewParameters = [];\n" );
-        if ( $this->hasAttribute( 'parameters' ) ) {
-            $viewParameters = $this->getAttribute( 'parameters' );
-            $compiler->write( "\$viewParameters = " );
-            $viewParameters->compile( $compiler );
-            $compiler->write( ";\n" );
+            if ( $lastAttributeName !== $attributeName ) {
+                $compiler->write( ',' );
+            }
         }
+        $compiler->write( "];\n" );
 
-        // Add lookups
-        $compiler->write( "\$viewLookups = [];\n" );
-        if ( $this->hasAttribute( 'lookups' ) ) {
-            $viewLookups = $this->getAttribute( 'lookups' );
-            $compiler->write( "\$viewLookups = " );
-            $viewLookups->compile( $compiler );
-            $compiler->write( ";\n" );
-        }
-
-        $compiler
-            ->write( "\$view = \\AlexaCRM\\WordpressCRM\\View::getViewForEntity(\$entityName, \$viewName);\n")
-            ->write( "if(\$view === null) {\n" )
-            ->indent()
-                ->write( "throw new \\Exception('Specified view not found');\n" )
-            ->outdent()
-            ->write( "}\n" );
-
-        $compiler->write( "list( \$fetchxml, \$layoutxml ) = [ \$view->fetchxml, \$view->layoutxml ];\n" );
-
-        // Substitute parameters
-        $compiler
-            ->write( "\$fetchxml = \\AlexaCRM\\WordpressCRM\\FetchXML::replacePlaceholderValuesByParametersArray(")
-            ->write( "\$fetchxml, \$viewParameters);\n" );
-
-        // Substitute lookups
-        $compiler
-            ->write( "\$fetchxml = \\AlexaCRM\\WordpressCRM\\FetchXML::replaceLookupConditionsByLookupsArray(" )
-            ->write( "\$fetchxml, \$viewLookups);\n");
-
-        // Add count/page attributes to the FetchXML
-        if ( $this->hasAttribute( 'count' ) ) {
-            $compiler->write( "\$viewCount = (int)" );
-            $this->getAttribute( 'count' )->compile( $compiler );
-            $compiler->write( ";\n" );
-            $compiler->write( "\$fetchxml = str_replace( '<fetch ', '<fetch count=\"' . \$viewCount . '\" ', \$fetchxml );\n" );
-            $compiler->write( "if ( \$viewCount > 1 ) {\n" )
-                ->indent()
-                ->write( "\$viewPageNumber = ACRM()->request->query->getInt( 'viewPage', 1 );\n" )
-                ->write( "\$fetchxml = str_replace( '<fetch ', '<fetch page=\"' . \$viewPageNumber . '\" ', \$fetchxml );\n" )
-                ->outdent()
-                ->write( "}\n" );
-        }
-
-        // Retrieve records
-        if ( $this->hasAttribute( 'cache' ) ) {
-            $compiler->write( "\$cache = ACRM()->getCache();\n" );
-            $compiler->write( "\$cacheKey = 'wpcrm_twigdata_' . sha1(\$fetchxml);\n" );
-            $compiler->write( "\$retrieveResult = \$cache->get(\$cacheKey);\n" );
-            $compiler->write( "if(\$retrieveResult === null){\n" );
-            $compiler->indent();
-        }
-        $compiler->write( "\$retrieveResult = ASDK()->retrieveMultiple(\$fetchxml);\n");
-
-        if ( $this->hasAttribute( 'cache' ) ) {
-            $intervalNode = $this->getAttribute( 'cache' );
-            $compiler->write( "\$interval = new \\DateInterval(" );
-            $intervalNode->compile( $compiler );
-            $compiler->write( ");\n" );
-            $compiler->write( "\$seconds = \$interval->y * YEAR_IN_SECONDS + \$interval->m * MONTH_IN_SECONDS + \$interval->d * DAY_IN_SECONDS + \$interval->h * HOUR_IN_SECONDS + \$interval->i * MINUTE_IN_SECONDS + \$interval->s;" );
-            $compiler->write( "\$cache->set(\$cacheKey, \$retrieveResult, \$seconds);\n" );
-            $compiler->outdent();
-            $compiler->write( "}\n" );
-        }
-
-        $compiler->write( "\$viewLayout = new SimpleXMLElement(\$layoutxml);\n" );
-        $compiler->write( "\$rawCells  = \$viewLayout->xpath( './/cell' );\n");
-        $compiler->write( "\$viewCells = [];\n" );
-        $compiler->write( "foreach ( \$rawCells as \$cell ) {" )
-            ->indent()
-            ->write( "\$viewCells[(string)\$cell['name']] = \$cell;\n" )
-            ->outdent()
-            ->write( "}\n" );
-
-        $compiler->write( "\$viewRows = \\AlexaCRM\\WordpressCRM\\View::getViewRows(\$retrieveResult," )
-            ->write( "\$viewCells,\$fetchxml);\n" );
-
-        $compiler->write( "\$context['_columns'] = [];\n" );
-        $compiler->write( "foreach ( reset(\$viewRows) as \$columnName => \$column ) {" )
-            ->indent()
-            ->write( "\$context['_columns'][\$columnName] = \$column['head'];\n" )
-            ->outdent()
-            ->write( "}\n" );
-
-        $compiler->write( "\$context['_records'] = \$viewRows;\n" );
+        $compiler->write( "\$viewBuilder = new \\AlexaCRM\\WordpressCRM\\ViewBuilder(\$viewAttributes);\n" );
+        $compiler->write( "\$context['entityview'] = \$viewBuilder->build();\n" );
         $compiler->subcompile( $this->getNode( 'template' ) );
-
-        $compiler->write( "unset(\$context['_columns'], \$context['_records']);\n");
     }
 
 }
