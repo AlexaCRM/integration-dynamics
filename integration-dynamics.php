dce2f1f1f3f9ae57b6b51ce4fac92d75417498b8
Pavel Selitskas <p.selitskas@gmail.com>
2017-05-19 12:37:28 +0300

    Remove php-soap dependency

--- integration-dynamics.php
+++ integration-dynamics.php
@@ -111,26 +111,4 @@ if ( !function_exists( 'curl_version' ) ) {
     return;
 }
 
-/**
- * Check whether php-soap is installed.
- */
-if ( !class_exists( '\\SoapFault' ) ) {
-    $logger->critical( 'php-soap is not installed. Cannot proceed further.' );
-
-    add_action( 'admin_notices', function() {
-        $screen = get_current_screen();
-        if ( $screen->base === 'plugins' ) {
-            ?>
-            <div class="notice notice-error">
-                <p>
-                    <?php _e( 'SOAP, a PHP extension, is not installed. <strong>Dynamics 365 Integration</strong> requires SOAP to work properly.', 'integration-dynamics' ); ?>
-                </p>
-            </div>
-            <?php
-        }
-    } );
-
-    return;
-}
-
 require_once __DIR__ . '/core.php';
