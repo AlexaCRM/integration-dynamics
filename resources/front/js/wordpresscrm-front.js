78b954485f227153423005e94c55f0a4514b3883
Pavel Selitskas <p.selitskas@gmail.com>
2018-11-15 14:38:00 +0300

    Treat email addresses with capital letters as valid

--- resources/front/js/wordpresscrm-front.js
+++ resources/front/js/wordpresscrm-front.js
@@ -1,6 +1,5 @@
-( function( $ ) {
-    "use strict";
-
+"use strict";
+( function( $, wpcrm, validationRules ) {
     var validationSettings = {
         highlight: function( element, errorClass ) {
             var $element = $( element ),
@@ -31,23 +30,23 @@
     $( function() {
         $( '.crm-datepicker' ).datetimepicker( {
             timepicker: false,
-            format: window.wpcrm.dateformat
+            format: wpcrm.dateformat
         } );
 
         $( '.crm-datetimepicker' ).datetimepicker( {
-            format: window.wpcrm.datetimeformat
+            format: wpcrm.datetimeformat
         } );
 
         // customize email validation regex
         $.validator.methods.email = function( value, element ) {
-            return this.optional( element ) || /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test( value );
+            return this.optional( element ) || /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/i.test( value );
         };
 
-        if ( window.wpcrmValidationSettings ) {
-            $.each( window.wpcrmValidationSettings, function( formId, settings ) {
+        if ( validationRules ) {
+            $.each( validationRules, function( formId, settings ) {
                 $( '#' + formId ).validate( $.extend( {}, settings, validationSettings ) );
             } );
         }
     } );
 
-} )( jQuery );
+} )( jQuery, window.wpcrm, window.wpcrmValidationSettings );
